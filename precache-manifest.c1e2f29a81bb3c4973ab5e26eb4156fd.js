abd3f3ff56eff2987f7e3cbb6120487e98696282
Tekena Solomon <tekena157@gmail.com>
2020-09-08 15:04:55 +0100

    Updates

---
+++ precache-manifest.c1e2f29a81bb3c4973ab5e26eb4156fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e33e48b2436900f58dd63bb28638ac2",
+    "url": "/ksih-qr-twitter/index.html"
+  },
+  {
+    "revision": "80f52b935ded431e4009",
+    "url": "/ksih-qr-twitter/static/css/main.7171a68b.chunk.css"
+  },
+  {
+    "revision": "2b466d7e8883e4897a26",
+    "url": "/ksih-qr-twitter/static/js/2.cfd7d1c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ksih-qr-twitter/static/js/2.cfd7d1c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80f52b935ded431e4009",
+    "url": "/ksih-qr-twitter/static/js/main.bd01cb9c.chunk.js"
+  },
+  {
+    "revision": "ab1ec1dc2e094ccac0f1",
+    "url": "/ksih-qr-twitter/static/js/runtime-main.faeb589a.js"
+  }
+]);
