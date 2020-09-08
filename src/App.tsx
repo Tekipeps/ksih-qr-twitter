62630104b323c25ab3a4d912ad8fa3799e064cba
Tekena Solomon <tekena157@gmail.com>
2020-09-08 14:54:17 +0100

    done with app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const App = () => {
+  const size = window.innerWidth < 500 ? 200 : 300;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1 className="heading">Ken Saro-Wiwa Innovation Hub</h1>
+      </div>
+      <h3>Twitter Qr Code</h3>
+      <div>
+        <img
+          src={`https://chart.googleapis.com/chart?cht=qr&chl=https%3A%2F%2Ftwitter.com%2Fintent%2Ftweet%3Ftext%3D%2523arvrPH%2520%2523ksih%2520%2523imisi3d%2520%2523arvrafrica%2520%250ATag%2520%250A%40imisi3d%2520%40ksih%2520%40arvrafrica&chs=${size}&choe=UTF-8&chld=L|2`}
+          alt="qr code"
+          className="qr-code"
+        />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
